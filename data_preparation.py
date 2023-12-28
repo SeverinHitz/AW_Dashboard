b72dc86322f7c7b676056a322fcdd3d57e7d4d74
SeverinHitz <sevihitz@bluewin.ch>
2023-12-28 20:17:19 +0100

    First Try

--- data_preparation.py
+++ data_preparation.py
@@ -1,3 +1,158 @@
 import os
 import pandas as pd
 
+# Disable the SettingWithCopyWarning
+pd.options.mode.chained_assignment = None
+
+def load_data():
+    # Set directory
+    program_path = os.path.realpath(__file__)
+    cwd = os.path.dirname(program_path)
+    data_dir = os.path.join(cwd, 'data')
+
+    # Import Data from Excel File
+    # Set Paths
+    flight_log_path = os.path.join(data_dir, '231228_flightlog.xlsx')
+    instructor_log_path = os.path.join(data_dir, '231228_instructorlog.xlsx')
+
+    fligth_df = pd.read_excel(flight_log_path)
+    instructor_df = pd.read_excel(instructor_log_path)
+
+    return fligth_df, instructor_df
+
+
+def data_cleanup(df, data_type):
+    if data_type == 'flightlog':
+        df = data_cleanup_flightlog(df)
+    elif data_type == 'instructorlog':
+        df = data_cleanup_instructorlog(df)
+    else:
+        raise 'Incorrect Datatype. Must be flightlog or instructorlog.'
+        df = None
+    return df
+
+def data_cleanup_flightlog(df):
+    # Select Columns
+    df = df[['Datum', 'Vorname', 'Name', 'Abflugort', 'Ankunftsort',\
+        'FlugzeitFlugzeit', 'Block Zeit', 'Benzin', 'Öl', 'Landungen',\
+        'Flugart', 'Flugzeug']]
+
+    # Rename Columns
+    column_mapping = {
+        'Datum': 'Date',
+        'Vorname': 'First Name',
+        'Name': 'Last Name',
+        'Abflugort': 'Departure Location',
+        'Ankunftsort': 'Arrival Location',
+        'FlugzeitFlugzeit': 'Flight Time',
+        'Block Zeit': 'Block Time',
+        'Benzin': 'Fuel',
+        'Öl': 'Oil',
+        'Landungen': 'Landings',
+        'Flugart': 'Flight Type',
+        'Flugzeug': 'Aircraft'
+    }
+    df.rename(columns=column_mapping, inplace=True)
+
+    # Pilot Full Name as Column
+    df['Pilot'] = df['First Name'] + ' ' + df['Last Name']
+
+    # Set Time as Timedelta
+    df['Flight Time'] = pd.to_timedelta(df['Flight Time'].astype(str))
+    df['Block Time'] = pd.to_timedelta(df['Block Time'].astype(str))
+    return df
+
+def data_cleanup_instructorlog(df):
+    # Select Columns
+    df = df[['Datum', 'Pilot Vorname', 'Pilot Name', 'Fluglehrer Vorname', 'Fluglehrer Name',\
+        'Dauer']]
+
+    # Rename Columns
+    column_mapping = {
+        'Datum': 'Date',
+        'Pilot Vorname': 'Pilot First Name',
+        'Pilot Name': 'Pilot Last Name',
+        'Fluglehrer Vorname': 'Instructor First Name',
+        'Fluglehrer Name': 'Instructor Last Name',
+        'Dauer': 'Duration'
+    }
+    df.rename(columns=column_mapping, inplace=True)
+
+    # Pilot and Instructor Full Name as Column
+    df['Pilot'] = df['Pilot First Name'] + ' ' + df['Pilot Last Name']
+    df['Instructor'] = df['Instructor First Name'] + ' ' + df['Instructor Last Name']
+
+    # Set Time as Timedelta
+    df['Duration'] = pd.to_timedelta(df['Duration'].astype(str))
+    return df
+
+def pilot_aggregation(df, start_date, end_date, sort_column='Total_Flight_Time'):
+    df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+    agg_df = df.groupby('Pilot').agg(
+        Total_Flight_Time=pd.NamedAgg(column='Flight Time', aggfunc='sum'),
+        Total_Block_Time=pd.NamedAgg(column='Block Time', aggfunc='sum'),
+        Number_of_Landings=pd.NamedAgg(column='Landings', aggfunc='sum'),
+        Number_of_Different_Airports=pd.NamedAgg(column='Arrival Location', aggfunc=lambda x: x.nunique()),
+        Number_of_Flights=pd.NamedAgg(column='Flight Time', aggfunc='count'),
+    )
+    agg_df['Total_Flight_Time'] = agg_df['Total_Flight_Time'].dt.total_seconds() / 3600
+    agg_df['Total_Block_Time'] = agg_df['Total_Block_Time'].dt.total_seconds() / 3600
+
+    agg_df.sort_values(sort_column, ascending=False, inplace=True)
+    agg_df.reset_index(inplace=True)
+    return agg_df
+
+def instructor_aggregation(df, start_date, end_date):
+    df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+    agg_df = df.groupby('Instructor').agg(
+        Total_Instructor_Time=pd.NamedAgg(column='Duration', aggfunc='sum'),
+        Number_of_Different_Pilots=pd.NamedAgg(column='Pilot', aggfunc=lambda x: x.nunique()),
+        Number_of_Instructions=pd.NamedAgg(column='Duration', aggfunc='count'),
+    )
+    agg_df['Total_Instructor_Time'] = agg_df['Total_Instructor_Time'].dt.total_seconds() / 3600
+    agg_df.sort_values('Total_Instructor_Time', ascending=False, inplace=True)
+    agg_df.reset_index(inplace=True)
+    return agg_df
+
+def aircraft_aggregation(df, start_date, end_date):
+    df = df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+    agg_df = df.groupby('Aircraft').agg(
+        Total_Flight_Time=pd.NamedAgg(column='Flight Time', aggfunc='sum'),
+        Number_of_Landings=pd.NamedAgg(column='Landings', aggfunc='sum'),
+        Number_of_Different_Airports=pd.NamedAgg(column='Arrival Location', aggfunc=lambda x: x.nunique()),
+        Number_of_Flights=pd.NamedAgg(column='Flight Time', aggfunc='count'),
+    )
+    agg_df['Total_Flight_Time'] = agg_df['Total_Flight_Time'].dt.total_seconds() / 3600
+
+    agg_df.sort_values('Total_Flight_Time', ascending=False, inplace=True)
+    agg_df.reset_index(inplace=True)
+    return agg_df
+
+
+if __name__ == '__main__':
+    flight_df, instructor_df = load_data()
+
+    #print(list(flight_df))
+    #print(list(instructor_df))
+
+    flight_df = data_cleanup(flight_df, 'flightlog')
+    instructor_df = data_cleanup(instructor_df, 'instructorlog')
+
+    print(list(flight_df))
+    print(list(instructor_df))
+    start_date = flight_df['Date'].min()
+    end_date = flight_df['Date'].max()
+    print(start_date)
+    print(end_date)
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    print(start_date)
+    print(end_date)
+    agg_flight_df = pilot_aggregation(flight_df, start_date, end_date)
+    agg_instructor_df = instructor_aggregation(instructor_df, start_date, end_date)
+
+    print(agg_flight_df)
+    print(agg_instructor_df)
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,107 @@
-# This is a sample Python script.
+# Import Libraries
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+from datetime import timedelta
+import dash
+from dash import Dash, dcc, html, dcc, callback, Input, Output
+from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import logging
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)s - %(message)s')
+
+# Import other Files
+import data_preparation as dp
+import html_functions as html_func
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+# Import Dataframes
+flight_df, instructor_df = dp.load_data()
+flight_df = dp.data_cleanup(flight_df, 'flightlog')
+instructor_df = dp.data_cleanup(instructor_df, 'instructorlog')
+
+start_date=flight_df['Date'].min()
+end_date=flight_df['Date'].max()
+
+agg_pilot_df = dp.pilot_aggregation(flight_df, start_date, end_date)
+agg_instructor_df = dp.instructor_aggregation(instructor_df, start_date, end_date)
+agg_aircraft_df = dp.aircraft_aggregation(flight_df, start_date, end_date)
+col_agg_pilot_df = list(agg_pilot_df)
+
+# Build App
+app = Dash(external_stylesheets=[dbc.themes.SLATE])
+
+app.layout = html.Div([
+    dbc.Card(
+        dbc.CardBody([
+            dbc.Row([
+                dcc.DatePickerRange(
+                    id='date-picker-range',
+                    start_date=flight_df['Date'].min(),
+                    end_date=flight_df['Date'].max(),
+                    display_format='YYYY-MM-DD'
+                ),
+            ], align='center'),
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    html_func.drawcenterText('Flightlog')
+                ], width=8),
+                dbc.Col([
+                    html_func.drawcenterText('Instructorlog')
+                ], width=4)
+            ], align='center'),
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    dcc.Dropdown(col_agg_pilot_df, 'Pilot', id='pilot_dropdown_x'),
+                    dcc.Dropdown(col_agg_pilot_df, 'Total_Flight_Time', id='pilot_dropdown_y'),
+                    dcc.Graph(id='main_flightlog_plot')
+                ], width=8),
+                dbc.Col([
+                    dcc.Graph(id='main_instructorlog_plot')
+                ], width=4)
+            ], align='center'),
+            html.Br(),
+            dbc.Row([
+                dbc.Col([
+                    dcc.Graph(id='aircraft_plot')
+                ], width=4),
+                dbc.Col([
+                    html_func.drawcenterText('Flightlog')
+                ], width=4),
+            ], align='center'),
+        ]), color = 'dark'
+    )
+])
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# Callback for updating the graphs
+@app.callback(
+    [Output('main_flightlog_plot', 'figure'),
+     Output('main_instructorlog_plot', 'figure'),
+     Output('aircraft_plot', 'figure')],
+    [Input('date-picker-range', 'start_date'),
+     Input('date-picker-range', 'end_date'),
+     Input('pilot_dropdown_x', 'value'),
+     Input('pilot_dropdown_y', 'value')]
+)
+def update_graphs(start_date, end_date, pilot_dropdown_x_value, pilot_dropdown_y_value):
+    print('X-Selection: ', pilot_dropdown_x_value)
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    # Filter data based on the selected date range
+    agg_pilot_df = dp.pilot_aggregation(flight_df, start_date, end_date, pilot_dropdown_y_value)
+    agg_instructor_df = dp.instructor_aggregation(instructor_df, start_date, end_date)
+    agg_aircraft_df = dp.aircraft_aggregation(flight_df, start_date, end_date)
+    # Create plots
+    fig1 = px.bar(agg_pilot_df, pilot_dropdown_x_value, pilot_dropdown_y_value)
+    fig2 = px.bar(agg_instructor_df, "Instructor", "Total_Instructor_Time")
+    fig3 = px.bar(agg_aircraft_df, "Aircraft", "Total_Flight_Time")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    return fig1, fig2, fig3
+
+
+# Run app and display result inline in the notebook
+app.run_server()
