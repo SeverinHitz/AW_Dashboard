97184cc32e9fedb7cd4ed242610551fde0c706d4
SeverinHitz <sevihitz@bluewin.ch>
2024-01-17 16:59:11 +0100

    Multi Page, Pilots and Basic Layout of Aircraft

--- pages/aircraft.py
+++ pages/aircraft.py
@@ -1,12 +1,173 @@
-# Aircrafts page
+# Aircraft page
 
 # Libraries
 from icecream import ic
 import dash
-from dash import html
+from dash import Dash, dcc, html, callback, Input, Output, dash_table
+import dash_bootstrap_components as dbc
+import pandas as pd
+import plotly.express as px
+import numpy as np
+# Import other Files
+import data_preparation as dp
+import layout as lo
+import plot as plots
+
+# Layout
+stylesheet = dbc.themes.SLATE #YETI
+layout_color = 'dark' #None
+plot_template = 'plotly_dark' #'plotly_white'
+color_scale = 'teal'
+plot_margin = dict(l=5, r=5, t=15, b=5)
+paper_bgcolor = 'rgba(0,0,0,0)'
+plot_window_style = { 'border-radius':'5px', 'background-color':'None'}
+discrete_teal = ['#2c5977', '#3a718d', '#4f90a6', '#62a5b4', '#7dbdc4', '#8fcacd', '#a1d7d6', '#E4FFFF', '#cfede9']
+grid_color = 'lightgrey'
+legend = dict(yanchor="top", y=0.99, xanchor="left", x=0.01, bgcolor='rgba(255, 255, 255, 0.2)')
+
+# Path
+flightlog_file = '240113_flightlog.xlsx'
+instructorlog_file = '240113_instructorlog.xlsx'
+reservationlog_file = '240113_reservationlog.xlsx'
+
+# Import Dataframes
+flight_df = dp.load_data(flightlog_file)
+reservation_df = dp.load_data(reservationlog_file)
+
+# clean up
+flight_df = dp.data_cleanup_flightlog(flight_df)
+reservation_df = dp.data_cleanup_reservation(reservation_df)
+
+pilots = flight_df['Aircraft'].sort_values().unique()
+# Append '⌀ All Pilots' to the array of unique pilot names
+pilots = np.append(pilots, '⌀ All Aircrafts')
 
 dash.register_page(__name__, path='/aircrafts', name='Aircrafts')
 
+
 layout = html.Div([
-    html.H2('Aircrafts Page')
+    dbc.Row([
+        dcc.Dropdown(pilots, '⌀ All Aircrafts', id='Aircraft-Dropdown')
+    ]),
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Aircaft"),
+            dbc.CardBody(
+            [
+                html.H4("Name", id='Aircraft-Registration'),
+            ]
+        )
+        ])
+        ], width=3),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Flight Time"),
+            dbc.CardBody(
+            [
+                html.H4("XXX h", id='Aircraft-Flight-Hours'),
+            ]
+        )
+        ])
+        ], width=1),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Flights"),
+                      dbc.CardBody(
+                          [
+                              html.H4("XXX #", id='Aircraft-Number-of-Flights'),
+                          ]
+                      )
+                      ])
+        ], width=1),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("⌀ Flt Time"),
+                      dbc.CardBody(
+                          [
+                              html.H4("XXX %", id='Aircraft-Mean-Flight-Time'),
+                          ]
+                      )
+                      ])
+        ], width=1),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Landings"),
+                      dbc.CardBody(
+                          [
+                              html.H4("XXX #", id='Aircraft-Number-of-Landings'),
+                          ]
+                      )
+                      ])
+        ], width=1),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Airports"),
+                      dbc.CardBody(
+                          [
+                              html.H4("XXX #", id='Aircraft-Number-of-Airports'),
+                          ]
+                      )
+                      ])
+        ], width=1),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Fuel p. h."),
+            dbc.CardBody(
+            [
+                html.H4("XXX L", id='Aircraft-Fuel-per-Hour'),
+            ]
+        )
+        ])
+        ], width=1),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Oil p. h."),
+            dbc.CardBody(
+            [
+                html.H4("XXX mL", id='Oil-per-Hour'),
+            ]
+        )
+        ])
+        ], width=1),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Inst. Ratio"),
+                      dbc.CardBody(
+                          [
+                              html.H4("XXX %", id='Aircraft-Instruction-Ratio'),
+                          ]
+                      )
+                      ])
+        ], width=1),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("# Pilots"),
+            dbc.CardBody(
+            [
+                html.H4("XXX #", id='Oil-per-Hour'),
+            ]
+        )
+        ])
+        ], width=1),
+    ], className="g-0"),
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Flight Time"),
+                      dbc.CardBody(
+                          [
+                              dcc.Graph(id='Aircraft-Flight-Time-Plot'),
+                          ]
+                      )
+                      ])
+        ], width=4),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Flight Type"),
+                      dbc.CardBody(
+                          [
+                              dcc.Graph(id='Aircraft-Flight-Type-Plot'),
+                          ]
+                      )
+                      ])
+        ], width=4),
+        dbc.Col([
+            dbc.Card([dbc.CardHeader("Heatmap"),
+                      dbc.CardBody(
+                          [
+                              dcc.Graph(id='Aircraft-Heatmap'),
+                          ]
+                      )
+                      ])
+        ], width=4)
+    ], className="g-0")
 ])
--- pages/pilots.py
+++ pages/pilots.py
@@ -7,6 +7,7 @@ from dash import Dash, dcc, html, callback, Input, Output, dash_table
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
+import numpy as np
 # Import other Files
 import data_preparation as dp
 import layout as lo
@@ -22,7 +23,7 @@ paper_bgcolor = 'rgba(0,0,0,0)'
 plot_window_style = { 'border-radius':'5px', 'background-color':'None'}
 discrete_teal = ['#2c5977', '#3a718d', '#4f90a6', '#62a5b4', '#7dbdc4', '#8fcacd', '#a1d7d6', '#E4FFFF', '#cfede9']
 grid_color = 'lightgrey'
-legend = dict(yanchor="top", y=0.99, xanchor="left", x=0.01)
+legend = dict(yanchor="top", y=0.99, xanchor="left", x=0.01, bgcolor='rgba(255, 255, 255, 0.2)')
 
 # Path
 flightlog_file = '240113_flightlog.xlsx'
@@ -31,22 +32,21 @@ reservationlog_file = '240113_reservationlog.xlsx'
 
 # Import Dataframes
 flight_df = dp.load_data(flightlog_file)
-instructor_df = dp.load_data(instructorlog_file)
 reservation_df = dp.load_data(reservationlog_file)
 
 # clean up
 flight_df = dp.data_cleanup_flightlog(flight_df)
-instructor_df = dp.data_cleanup_instructorlog(instructor_df)
 reservation_df = dp.data_cleanup_reservation(reservation_df)
 
 pilots = flight_df['Pilot'].sort_values().unique()
-
+# Append '⌀ All Pilots' to the array of unique pilot names
+pilots = np.append(pilots, '⌀ All Pilots')
 
 dash.register_page(__name__, path='/pilots', name='Pilots')
 
 layout = html.Div([
     dbc.Row([
-        dcc.Dropdown(pilots, 'Severin Hitz', id='Pilot-Dropdown')
+        dcc.Dropdown(pilots, '⌀ All Pilots', id='Pilot-Dropdown')
     ]),
     dbc.Row([
         dbc.Col([
@@ -145,20 +145,20 @@ layout = html.Div([
             dbc.Card([dbc.CardHeader("Flight Time"),
                       dbc.CardBody(
                           [
-                              dcc.Graph(id='res1'),
+                              dcc.Graph(id='Pilots-Flight-Time-Plot'),
                           ]
                       )
                       ])
-        ], width=6),
+        ], width=8),
         dbc.Col([
-            dbc.Card([dbc.CardHeader("Instruction Time"),
+            dbc.Card([dbc.CardHeader("Cancellation Reason"),
                       dbc.CardBody(
                           [
-                              dcc.Graph(id='res2'),
+                              dcc.Graph(id='Pilot-Cancel-Reason'),
                           ]
                       )
                       ])
-        ], width=6)
+        ], width=4)
     ], className="g-0")
 ])
 
@@ -191,37 +191,106 @@ def update_pilots_header(start_date, end_date, pilot_dropdown):
     # Aggregate Pilots Data
     agg_pilot_df = dp.pilot_aggregation(filtered_flight_df)
 
-    agg_pilot_reservation = agg_reservation_df[agg_reservation_df['Pilot']==pilot_dropdown]
-    agg_pilot_flight = agg_pilot_df[agg_pilot_df['Pilot']==pilot_dropdown]
-    str = f'XYZ'
-
-    agg_flight_res_df = dp.reservation_flight_merge(agg_pilot_reservation, agg_pilot_flight)
+    agg_flight_res_df = dp.reservation_flight_merge(agg_reservation_df, agg_pilot_df)
+    if pilot_dropdown == '⌀ All Pilots':
+        agg_flight_res_df = agg_flight_res_df.iloc[:, 1:].mean().to_frame().T
+    else:
+        agg_flight_res_df = agg_flight_res_df[agg_flight_res_df['Pilot']==pilot_dropdown]
 
     if len(agg_flight_res_df)==1:
         # Pilots Flight Time
         sum_flight_time = f'{agg_flight_res_df.iloc[0]["Total_Flight_Time"]:.2f} h'
         # Pilots Number of Flights
-        sum_block_time = f'{agg_flight_res_df.iloc[0]['Number_of_Flights']:.2f} h'
+        sum_block_time = f'{agg_flight_res_df.iloc[0]['Total_Block_Time']:.2f} h'
         # Pilots Flight to Block Time
         flight_block_ratio = f'{agg_flight_res_df.iloc[0]['Flight_Block_Ratio']*100:.2f} %'
         # Pilots Number of Flights
-        sum_flights = f'{agg_flight_res_df.iloc[0]['Number_of_Flights']:.2f} #'
+        sum_flights = f'{agg_flight_res_df.iloc[0]['Number_of_Flights']:.0f} #'
         # Pilots Landings
-        sum_landings = f'{agg_flight_res_df.iloc[0]['Number_of_Landings']:.2f} #'
+        sum_landings = f'{agg_flight_res_df.iloc[0]['Number_of_Landings']:.0f} #'
         # Pilots Reservations to Flighttime
         res_flight_time = f'{agg_flight_res_df.iloc[0]['Flight_to_Reservation_Time']*100:.2f} %'
         # Pilots Cancelled Reservation
-        reservations = f'{agg_flight_res_df.iloc[0]['Reservations']:.2f} #'
+        reservations = f'{agg_flight_res_df.iloc[0]['Reservations']:.0f} #'
         # Pilots Cancelled Reservation
-        cancelled = f'{agg_flight_res_df.iloc[0]['Cancelled']:.2f} #'
+        cancelled = f'{agg_flight_res_df.iloc[0]['Cancelled']:.0f} #'
         # Pilots Cancelled Ratio
         cancelled_ratio = f'{agg_flight_res_df.iloc[0]['Ratio_Cancelled']*100:.2f} %'
     else:
         pilot_dropdown, sum_flight_time, sum_block_time, flight_block_ratio, \
             sum_flights, sum_landings, res_flight_time, reservations, cancelled, cancelled_ratio = ('NO DATA',) * 10
 
+    return pilot_dropdown, sum_flight_time, sum_block_time, flight_block_ratio,\
+        sum_flights, sum_landings, res_flight_time, reservations, cancelled, cancelled_ratio
 
 
-    return pilot_dropdown, sum_flight_time, sum_block_time, flight_block_ratio, sum_flights, sum_landings, res_flight_time, reservations, cancelled, cancelled_ratio
+@callback(
+    [Output('Pilots-Flight-Time-Plot', 'figure')],
+    [Input('date-picker-range', 'start_date'),
+     Input('date-picker-range', 'end_date'),
+     Input('Pilot-Dropdown', 'value')]
+)
+def update_pilot_graphs(start_date, end_date, pilot_dropdown):
+    # Set Date as a Datetime object
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    # Filter Flightlog data based on the selected date range
+    filtered_flight_df = dp.date_select_df(flight_df, start_date, end_date)
+    # Aggregate Pilots Data
+    agg_pilot_df = dp.pilot_aggregation(filtered_flight_df)
+    # Create Pilot Plot
+    pilots_flight_time_plot = px.bar(
+        agg_pilot_df,
+        'Pilot',
+        'Total_Flight_Time',
+        color='Total_Flight_Time',
+        template=plot_template,
+        color_continuous_scale=color_scale
+    )
+    # Update the color of the selected pilot in the bar plot
+    if pilot_dropdown != '⌀ All Pilots':
+        pilots_flight_time_plot.update_traces(
+            marker=dict(color=[discrete_teal[-1] if pilot == pilot_dropdown else discrete_teal[0] for pilot in agg_pilot_df['Pilot']]),
+            hovertext=agg_pilot_df['Total_Flight_Time'],
+            selector=dict(type='bar')
+        )
+    pilots_flight_time_plot.update(layout_coloraxis_showscale=False)
+    pilots_flight_time_plot.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey')
+    pilots_flight_time_plot.update_layout(margin=plot_margin,
+                                          paper_bgcolor=paper_bgcolor,
+                                          plot_bgcolor=paper_bgcolor)
 
+    return [pilots_flight_time_plot]
+
+@callback(
+    [Output('Pilot-Cancel-Reason', 'figure')],
+    [Input('date-picker-range', 'start_date'),
+     Input('date-picker-range', 'end_date'),
+     Input('Pilot-Dropdown', 'value')]
+)
+def update_reservation_graph(start_date, end_date, pilot_dropdown):
+    # Set Date as a Datetime object
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    # Filter Reservation data based on the selected date range
+    filtered_reservation_df = dp.date_select_df(reservation_df, start_date, end_date, date_column='From')
+    if pilot_dropdown != '⌀ All Pilots':
+        filtered_reservation_df = filtered_reservation_df[filtered_reservation_df['Pilot']==pilot_dropdown]
+    reservation_sum = filtered_reservation_df[filtered_reservation_df['Deleted']]['Deletion Reason'].value_counts()
+
+    # Create Reservation Plot
+    pilot_cancel_reason_plot = px.pie(
+        reservation_sum,
+        names=reservation_sum.index,
+        values='count',
+        template=plot_template,
+        color_discrete_sequence=discrete_teal
+    )
+    pilot_cancel_reason_plot.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey')
+    pilot_cancel_reason_plot.update_layout(margin=plot_margin,
+                                       paper_bgcolor=paper_bgcolor,
+                                       plot_bgcolor=paper_bgcolor,
+                                       legend=legend)
+
+    return [pilot_cancel_reason_plot]
 
